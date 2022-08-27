e65822a7e22f9145b9e41368171c278e2f0bd272
Vladyslav Liashuk <mystikhawk333@gmail.com>
2022-08-27 15:24:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        class MyDictionary<T> : IEnumerable, IEnumerator
+        {
+            Dictionary<T>[] dictionary = null;
+            Dictionary<T>[] dictionaryBuffer = null;
+            public void Add(int key, T value)
+            {
+                if (dictionary == null)
+                {
+                    dictionary = new Dictionary<T>[1];
+                    dictionary[0] = new Dictionary<T>(key, value);
+                }
+                else
+                {
+                    dictionaryBuffer = new Dictionary<T>[dictionary.Length + 1];
+                    for (int i = 0; i < dictionaryBuffer.Length; i++)
+                    {
+                        if (i < dictionary.Length)
+                            dictionaryBuffer[i] = dictionary[i];
+                        else
+                            dictionaryBuffer[i] = new Dictionary<T>(key, value);
+                    }
+                    dictionary = dictionaryBuffer;
+                    dictionaryBuffer = null;
+                }
+            }
+            public Dictionary<T> this[int index]
+            {
+                get
+                {
+                    if (dictionary != null)
+                    {
+                        if (index >= 0 && index < dictionary.Length)
+                        {
+                            return dictionary[index];
+                        }
+                        else
+                        {
+                            Console.WriteLine("Попытка обращения за пределы коллекции, возвращено стандартное значение value");
+                            return default(Dictionary<T>);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Обнаружена пустая коллекция, возвращено стандартное значение value");
+                        return default(Dictionary<T>);
+                    }
+                }
+                set
+                {
+                    if (dictionary != null)
+                    {
+                        if (index >= 0 && index < dictionary.Length)
+                            dictionary[index] = value;
+                        else
+                            Console.WriteLine("Попытка обращения за пределы коллекции, попробуйте другой индекс");
+                    }
+                    else
+                        Console.WriteLine("Обнаружена пустая коллекция, добавьте значение");
+                }
+            }
+            //public Dictionary<T> this[int key]
+            //{
+            //    get
+            //    {
+            //        if (dictionary != null)
+            //        {
+            //            for (int i = 0; i < dictionary.Length; i++)
+            //            {
+            //                if (key == dictionary[i].Key)
+            //                {
+            //                    return dictionary[i];
+            //                }
+            //            }
+            //            Console.WriteLine("Не найдено подходящего ключа, возвращено стандратное значение value");
+            //            return default(Dictionary<T>);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Обнаружена пустая коллекция, возвращено стандартное значение value");
+            //            return default(Dictionary<T>);
+            //        }
+            //    }
+            //    set
+            //    {
+            //        if (dictionary != null)
+            //        {
+            //            for (int i = 0; i < dictionary.Length; i++)
+            //            {
+            //                if (key == dictionary[i].Key)
+            //                {
+            //                    dictionary[i] = value;
+            //                }
+            //            }
+            //            Console.WriteLine("Попытка обращения за пределы коллекции, попробуйте другой индекс");
+            //        }
+            //        else
+            //            Console.WriteLine("Обнаружена пустая коллекция, добавьте значение");
+            //    }
+            //}
+            int pos = -1;
+            public bool MoveNext()
+            {
+                if(pos < dictionary.Length - 1)
+                {
+                    pos++;
+                    return true;
+                }
+                else
+                {
+                    Reset();
+                    return false;
+                }
+            }
+            public void Reset() { pos = -1; }
+            public object Current { get { return dictionary[pos]; } }
+            public IEnumerator GetEnumerator() { return this; }
+        }
+        static void Main(string[] args)
+        {
+            MyDictionary<string> dict = new MyDictionary<string>();
+            dict.Add(2, "Hello");
+            foreach (Dictionary<string> dictionary in dict)
+            {
+                Console.WriteLine("Key - {0}, Value - {1}", dictionary.Key, dictionary.Value);
+            }
+            Console.WriteLine();
+            dict.Add(4, "World");
+            foreach (Dictionary<string> dictionary in dict)
+            {
+                Console.WriteLine("Key - {0}, Value - {1}", dictionary.Key, dictionary.Value);
+            }
+            Console.ReadKey();
+        }
+        class Dictionary<T>
+        {
+            public int Key { get; set; }
+            public T Value { get; set; }
+            public Dictionary(int key, T value)
+            {
+                Key = key;
+                Value = value;
+            }
+        }
+    }
+}
---
+++ ДЗ.14/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static ДЗ._14.Program;
+
+namespace ДЗ._14
+{
+    static class Arrays
+    {
+        public static T[] GetArray<T>(this MyList list)
+        {
+            T[] result = new T[list.GetCollectionLength()];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = list[i];
+            }
+            return result;
+        }
+    }
+    internal class Program
+    {
+        public class Product
+        {
+            public string TypeOfProduct { get; set; }
+            public string Name { get; set; }
+            public double Price { get; set; }
+            public Product(string typeOfProduct, string name, double price)
+            {
+                TypeOfProduct = typeOfProduct;
+                Name = name;
+                Price = price;
+            }
+        }
+        public class MyList : IEnumerable, IEnumerator
+        {
+            public Product[] products = null;
+            public Product[] productsBoofer = null;
+            public void Add(string typeOfProduct, string name, double price)
+            {
+                if(products == null)
+                {
+                    products = new Product[1];
+                    products[0] = new Product(typeOfProduct, name, price);
+                }
+                else
+                {
+                    productsBoofer = new Product[products.Length + 1];
+                    for (int i = 0; i < productsBoofer.Length; i++)
+                    {
+                        if (i < products.Length)
+                            productsBoofer[i] = products[i];
+                        else
+                            productsBoofer[i] = new Product(typeOfProduct, name, price);
+                    }
+                    products = productsBoofer;
+                    productsBoofer = null;
+                }
+            }
+            public int GetCollectionLength()
+            {
+                return products.Length;
+            }
+            public Product this[int index]
+            {
+                get
+                {
+                    if(index > 0 && index < products.Length)
+                    {
+                        return products[index];
+                    }
+                    else
+                        return null;
+                }
+                set
+                {
+                    if (index > 0 && index < products.Length)
+                    {
+                        products[index] = value;
+                    }
+                    else
+                        Console.WriteLine("Попытка обращения за пределы коллекции");
+                }
+            }
+            int pos = -1;
+            public bool MoveNext()
+            {
+                if(pos < products.Length -1)
+                {
+                    pos++;
+                    return true;
+                }
+                else
+                {
+                    Reset();
+                    return false;
+                }
+            }
+            public void Reset()
+            {
+                pos = -1;
+            }
+            public object Current
+            {
+                get { return products[pos]; }
+            }
+            public IEnumerator GetEnumerator()
+            {
+                return this;
+            }
+        }
+        static void Main(string[] args)
+        {
+            MyList supermarket = new MyList();
+            supermarket.Add("Food", "Milk", 16.90);
+            foreach (Product products in supermarket)
+            {
+                Console.WriteLine("Type - {0}, Name - {1}, Price - {2}", products.TypeOfProduct, products.Name, products.Price);
+            }
+            supermarket.Add("Food", "Meat", 16.90);
+            Console.WriteLine();
+            foreach (Product products in supermarket)
+            {
+                Console.WriteLine("Type - {0}, Name - {1}, Price - {2}", products.TypeOfProduct, products.Name, products.Price);
+            }
+            Console.WriteLine();
+            Arrays.GetArray<Product>(supermarket);
+            Console.ReadKey();
+        }
+        
+    }
+}
